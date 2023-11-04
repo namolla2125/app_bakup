0ebaa0f31ad90aaf28894fbf83b6ec708d25139d
Namolla <kmc130710@daum.net>
2023-11-04 10:45:35 +0900

    aaa

--- js/bg_color_update.js
+++ js/bg_color_update.js
@@ -2,5 +2,5 @@ window.onload = function(){
   setTimeout(() => {
     var bgcolor = JSON.parse(localStorage.getItem("namollatoolbox-setting")).bg[0];
     document.querySelector("body").style.backgroundColor = bgcolor;
-  }, 1500)
+  }, 200)
 }
